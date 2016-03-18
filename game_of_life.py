6c693eaef85f9de9311d12b2f28a49a543e971d0
Juan Domenech Fernandez <juan.domenech@gmail.com>
2016-03-18 16:36:41 +0000

    Main functions ready a initial basic tests

--- game_of_life.py
+++ game_of_life.py
@@ -1,3 +1,168 @@
 
-def test(x): return x
+#
+# Die -> <2 neighbours
+# Die -> >3 neighbours
+# Born -> =3 neighbours
+# Survive -> =2 neighbours
+# Survive -> =3 neighbours
+#
+
+"""
+(-1,-1) (0,-1) (1,-1)
+
+(-1,0)  (0,0)  (1,0)
+
+(-1,1)  (0,1)  (1,1)
+
+"""
+
+DEBUG = False
+
+def next_generation(world):
+
+    new_world = []
+
+    if not world:
+        return new_world
+
+    if DEBUG:
+        print "Current Generation"
+        print_world(world)
+
+
+    for item in world:
+
+        # Get Neighbours
+        neighbours = get_neighbours(world,item)
+
+        # Die -> <2 neighbours
+        # Die -> >3 neighbours
+        if len(neighbours) < 2 or len(neighbours) > 3:
+            pass
+        else:
+            new_world.append(item)
+
+        # Survive -> =2 neighbours
+        # Survive -> =3 neighbours
+        if len(neighbours) == 2 or len(neighbours) == 3:
+            new_world.append(item)
+
+        # Born -> =3 neighbours
+        top_left,bottom_right = find_corners(world)
+
+        for y in range( top_left[1]-1, bottom_right[1] +2 ):
+
+            for x in range( top_left[0]-1, bottom_right[0] +2 ):
+
+                if len( get_neighbours(world, (x,y) ) ) == 3:
+                    new_world.append((x,y))
+                    if DEBUG:
+                        print "New cell: (",x,y,")"
+                #if (x,y) in world:
+                #    print "["+str(x)+","+str(y)+"]\t",
+                #else:
+                #    print "("+str(x)+","+str(y)+")\t",
+
+    new_world = sorted ( list(set( new_world )) )
+
+    #print
+    #print_world(new_world)
+
+    if len(new_world) == 0:
+        print "No cells survived!"
+
+    if DEBUG:
+        print new_world
+
+    return new_world
+
+
+def get_neighbours(world,cell):
+
+    neighbours = []
+    # (0,-1)
+    if (cell[0],cell[1]-1) in world:
+        neighbours.append( (cell[0],cell[1]-1) )
+    # (1,-1)
+    if (cell[0]+1,cell[1]-1) in world:
+        neighbours.append( (cell[0]+1,cell[1]-1) )
+    # (1,0)
+    if (cell[0]+1,cell[1]) in world:
+        neighbours.append( (cell[0]+1,cell[1]) )
+    # (1,1)
+    if (cell[0]+1,cell[1]+1) in world:
+        neighbours.append( (cell[0]+1,cell[1]+1) )
+    # (0,1)
+    if (cell[0],cell[1]+1) in world:
+        neighbours.append( (cell[0],cell[1]+1) )
+    # (-1,1)
+    if (cell[0]-1,cell[1]+1) in world:
+        neighbours.append( (cell[0]-1,cell[1]+1) )
+    # (-1,0)
+    if (cell[0]-1,cell[1]) in world:
+        neighbours.append( (cell[0]-1,cell[1]) )
+    # (-1,-1)
+    if (cell[0]-1,cell[1]-1) in world:
+        neighbours.append( (cell[0]-1,cell[1]-1) )
+
+    if DEBUG:
+            print "Neighbours of ",cell," : ",neighbours
+
+    return neighbours
+
+
+def find_corners(world):
+    top_left =  (0,0)
+    bottom_right = (0,0)
+
+    for item in world:
+        if item[0] < top_left[0]:
+            top_left = item[0],top_left[1]
+        if item[1] < bottom_right[1]:
+            top_left = top_left[0],item[1]
+    if DEBUG:
+        print "Top Left",top_left
+
+    for item in world:
+        if item[0] > bottom_right[0]:
+            bottom_right = item[0],bottom_right[1]
+        if item[1] > bottom_right[1]:
+            bottom_right = bottom_right[0],item[1]
+    if DEBUG:
+        print "Bottom Right",bottom_right
+
+    return top_left,bottom_right
+
+
+
+def print_world(world):
+
+    # Find corners to know how big our world is
+    top_left,bottom_right = find_corners(world)
+
+    ex = ''
+
+    print 'World: Population:',len(world),"  X-Axis Size:', abs(top_left[0]) + abs(bottom_right[0]+1),'  Y-Axis Size:', abs(top_left[1]) + abs(bottom_right[1]+1)
+
+    for y in range( top_left[1], bottom_right[1] +1 ):
+
+        for x in range( top_left[0], bottom_right[0] +1 ):
+
+            if DEBUG:
+                if (x,y) in world:
+                    print "["+str(x)+","+str(y)+"]\t",
+                else:
+                    print "("+str(x)+","+str(y)+")\t",
+            else:
+                if (x,y) in world:
+                    ex += "* "
+                else:
+                    ex += ". "
+        print ex
+        ex = ''
+
+        print
+    print
+
+    return
 
--- test_game_of_life.py
+++ test_game_of_life.py
@@ -1,9 +1,54 @@
 import unittest
+import time
+import os
 
-from game_of_life import test
+from game_of_life import next_generation, print_world
+
+# Blinker (period 2)
+#world = [(1,0), (1,1), (1,2)]
+# R-Pentomino
+#world = [ (1,0),(2,0),(0,1),(1,1),(1,2) ]
+# Die Hard
+world =  [ (0,1),(1,1),(1,2),(6,0),(5,2),(6,2),(7,2) ]
+print "Generation # 0"
+print_world(world)
+time.sleep(1)
+
+for x in range(1,55555):
+
+    new_world = next_generation(world)
+    os.system('clear')
+    print "Generation #",x
+    print_world(new_world)
+
+    if len(new_world) == 0:
+        print "All dead at generation #",x
+        break
+    #time.sleep(5)
+    world = new_world
+
+
+
+
 
 class test_game_of_life(unittest.TestCase):
 
+    # Empty
     def test(self):
-        self.assertEquals(test(1),1)
-        
+        self.assertEquals( next_generation( [] ) , [] )
+
+    # Solitarie cell that dies
+    #def test(self):
+    #    self.assertEquals( next_generation( [(0,0)] ) , [] )
+
+    # Block (https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Examples_of_patterns)
+    #def test(self):
+    #    self.assertEquals( next_generation( sorted ( [(0,0), (0,1), (1,1), (1,0)] ) ) , sorted( [(0,0), (0,1), (1,1), (1,0)] ) )
+
+    # Beehive (https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Examples_of_patterns)
+    #def test(self):
+    #    self.assertEquals( next_generation( sorted ( [(1,0), (2,0), (0,1), (3,1), (1,2), (2,2)] ) ) , sorted( [(1,0), (2,0), (0,1), (3,1), (1,2), (2,2)] ) )
+
+    # Blinker (Period 2) (https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Examples_of_patterns)
+    #def test(self):
+    #    self.assertEquals( next_generation( sorted ( [(1,0), (1,1), (1,2)] ) ) , sorted( [(0,1), (1,1), (2,1)] ) )
